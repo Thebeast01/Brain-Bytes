640a8a01a8b58dc239b09b00722cb4bd13e26a75
harshitvermaa1 <121253585+harshitvermaa1@users.noreply.github.com>
2023-09-09 12:26:33 +0530

    FOOTER

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,16 @@
 import React, { Fragment } from "react";
-
+import Header from "../components/Header/Header";
 import Courses from "../components/Courses-section/Courses";
-
+// import Blog from "./components/blog/Blog"
 import Features from "../components/Feature-section/Features";
 
 import Footer from "../components/Footer/Footer";
 // import Testimonal from "../components/testimonal/Testimonal";
 const Home = () => {
   return (
+
     <Fragment>
+        <Header />
     <Courses />
       <Features />
       {/* <Testimonal/> */}
