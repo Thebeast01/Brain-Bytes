158ffad75b9f08b9999e775dcc0a25a88542c0a0
vikaskumar2703 <architectvikas272003@gmail.com>
2023-09-09 19:14:24 +0530

    Adds doubt page code

--- src/pages/Doubt.js
+++ src/pages/Doubt.js
@@ -1,10 +1,14 @@
 import "../index.css";
 import { DoubtForm } from "../components/DoubtForm";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer/Footer";
 
 export const Doubt = () => {
   return (
     <>
+      <Navbar />
       <DoubtForm />
+      <Footer />
     </>
   );
 };
