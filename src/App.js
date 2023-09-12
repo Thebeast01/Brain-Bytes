ee6cfd30bb013664922302157d2740922474ade0
Beast <mohammadsaif0847@gmail.com>
2023-09-12 17:55:08 +0530

    Login Page added By Farhan

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from './pages/Home';
@@ -7,20 +6,20 @@ import DoubtPage from './pages/Doubt';
 import CoursePage from './pages/Course-page/Course-page';
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route  exact path="/" component={HomePage} />
-        <Route path="/about" component={AboutPage} />
-        <Route path="/Doubt" component={DoubtPage} />
-        {/* <Route path="/Course" component={CoursePage} /> */}
-        <Route path="/courses/class6" component={CoursePage} />
-  <Route path="/courses/class7" component={CoursePage} />
-  <Route path="/courses/class8" component={CoursePage} />
-  <Route path="/courses/class9" component={CoursePage} />
-      </Switch>
-    </Router>
-  );
+	return (
+		<Router>
+			<Switch>
+				<Route exact path='/' component={HomePage} />
+				<Route path='/about' component={AboutPage} />
+				<Route path='/Doubt' component={DoubtPage} />
+				{/* <Route path="/Course" component={CoursePage} /> */}
+				<Route path='/courses/class6' component={CoursePage} />
+				<Route path='/courses/class7' component={CoursePage} />
+				<Route path='/courses/class8' component={CoursePage} />
+				<Route path='/courses/class9' component={CoursePage} />
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
