348d51843788343a4f0dba63d588645d8d5273cc
harshitvermaa1 <121253585+harshitvermaa1@users.noreply.github.com>
2023-09-12 12:50:20 +0530

    complete homepage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-// // import AboutUs from './components/AboutUs';
-// import Home from "./pages/Home";
-// // import Blog from "./components/blog/Blog"
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-// function App() {
-  
-//       // <Router>
-//       //   <Route exact path='/journal' component={Blog} />
-//       // </Router>
-//   return <Home />;
-//   // <Route exact path='/journal' component={Blog} />
-// }
 
-// export default App;
 
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from './pages/Home';
 import AboutPage from './pages/AboutUs';
-// import ContactPage from './ContactPage';
+
 
 function App() {
   return (
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,16 +4,11 @@ import Courses from "../components/Courses-section/Courses";
 
 
 import Features from "../components/Feature-section/Features";
-// import AboutUs from "../components/About-us/AboutUs";
-// import Footer from "../components/Footer/Footer";
-// import Landingpage from "../src/components/Landingpage/Landingpage";
-// import Testimonal from "../components/Testimonal/Testimonal";
 
-// import Testimonials from "../components/Testimonial/Testimonials";
-// import  Recentcourse from "../components/Recent-course/Recentcourse";
+
 import Slider from "../components/Slider/Slider";
 import Footer from "../components/Footer/Footer";
-// import Header from "../components/Header/Header";
+//
 import NotificationSection from "../components/notification/notification";
 import Testimonials from "../components/Testimonial/Testimonials";
 import  Recentcourse from "../components/Recent-course/Recentcourse";
@@ -24,12 +19,7 @@ const Home = () => {
 
     <Fragment>
 
-        {/* <Header /> */}
-        {/* <AboutUs />
-       
-    <Courses />
-      <Features />
-      <Testimonials/> */}
+      
 
         <Navbar/>
         
