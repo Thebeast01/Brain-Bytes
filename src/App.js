b4aec33667d23b44fb15418119fa45fed90999d2
Vikas Kumar <architectvikas272003@gmail.com>
2023-09-13 16:15:36 +0530

    Revert "Merge pull request #3 from harshitvermaa1/conflict-resolve"
    This reverts commit f9142f07dce6ffab2a7b3a3b3ef0d340e8f4e06e, reversing
    changes made to 2244d515dc10f36b6f826c0ef0b1d43080251c26.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+<<<<<<< HEAD
 
+import Home from './pages/Home';
+
+<<<<<<< HEAD
+=======
+// import AboutUs from './components/AboutUs';
+import Home from "./pages/Home";
+// import Blog from "./components/blog/Blog"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+>>>>>>> parent of f88ac45 (Merge branch 'main' into conflict-resolve)
+=======
+>>>>>>> parent of f9142f0 (Merge pull request #3 from harshitvermaa1/conflict-resolve)
 function App() {
-  
-      // <Router>
-      //   <Route exact path='/journal' component={Blog} />
-      // </Router>
   return <Home />;
-  // <Route exact path='/journal' component={Blog} />
 }
 
 export default App;
