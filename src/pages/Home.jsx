7d6041789660729cd209e8ebd14ed6f43c1e38db
Vikas Kumar <architectvikas272003@gmail.com>
2023-09-13 16:21:22 +0530

    Revert "Revert "Merge pull request #3 from harshitvermaa1/conflict-resolve""
    This reverts commit b4aec33667d23b44fb15418119fa45fed90999d2.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,51 +1,27 @@
 import React, { Fragment } from "react";
-<<<<<<< HEAD
-<<<<<<< HEAD
-import Navbar from "../components/Navbar";
-=======
-// import Navbar from "../components/Header/Header";
->>>>>>> parent of f88ac45 (Merge branch 'main' into conflict-resolve)
-=======
-import Navbar from "../components/Navbar";
->>>>>>> parent of f9142f0 (Merge pull request #3 from harshitvermaa1/conflict-resolve)
-import Courses from "../components/Courses-section/Courses";
-// import Blog from "./components/blog/Blog"
-import Features from "../components/Feature-section/Features";
-// import AboutUs from "../components/About-us/AboutUs";
-// import Footer from "../components/Footer/Footer";
-// import Landingpage from "../src/components/Landingpage/Landingpage";
-// import Testimonal from "../components/Testimonal/Testimonal";
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-// import Testimonials from "../components/Testimonial/Testimonials";
-// import  Recentcourse from "../components/Recent-course/Recentcourse";
+// import Navbar from "../components/Header/Header";
+
+import Navbar from "../components/Navbar";
+
+import Courses from "../components/Courses-section/Courses";
+
+
+import Features from "../components/Feature-section/Features";
+
 import Slider from "../components/Slider/Slider";
->>>>>>> parent of f88ac45 (Merge branch 'main' into conflict-resolve)
-=======
->>>>>>> parent of f9142f0 (Merge pull request #3 from harshitvermaa1/conflict-resolve)
 import Footer from "../components/Footer/Footer";
-// import Testimonal from "../components/testimonal/Testimonal";
+import Header from "../components/Header/Header";
+import NotificationSection from "../components/notification/notification";
+import Testimonials from "../components/Testimonial/Testimonials";
+import  Recentcourse from "../components/Recent-course/Recentcourse";
+import AboutUs from "../components/About-us/AboutUs";
+
 const Home = () => {
   return (
-<<<<<<< HEAD
-
     <Fragment>
-<<<<<<< HEAD
-      <Navbar />
-      <Courses />
-      <Features />
-      {/* <Testimonal/> */}
-      <Footer />
-=======
 
-        <Header />
-        <AboutUs />
-       
-    <Courses />
-      <Features />
-      <Testimonials/>
+\
 
         <Header/>
         <Slider />
@@ -59,15 +35,8 @@ const Home = () => {
       <Recentcourse />
    <Footer />
    
->>>>>>> parent of f88ac45 (Merge branch 'main' into conflict-resolve)
-=======
-    <Fragment>
-      <Navbar />
-      <Courses />
-      <Features />
-      {/* <Testimonal/> */}
-      <Footer />
->>>>>>> parent of f9142f0 (Merge pull request #3 from harshitvermaa1/conflict-resolve)
+
+  
     </Fragment>
   );
 };
